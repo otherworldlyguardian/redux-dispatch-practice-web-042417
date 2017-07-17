433a63091920c449cb6e7e5159e20672226951a0
otherworldlyguardian <otherworldlyguardian@gmail.com>
2017-07-17 16:04:21 -0400

    pet list

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,35 @@
 export let state;
 
 
-export function managePets(){
+export function managePets(state = {pets: []}, action){
+  switch (action.type) {
+    case 'ADD_PET':
+      return Object.assign({}, state, {
+        pets: [
+          ...state.pets,
+          action.pet
+        ]
+      })
+    case 'REMOVE_PET':
+      const index = state.pets.findIndex( pet => pet.id === action.id )
+      return Object.assign({}, state, {
+        pets: [
+          ...state.pets.slice(0, index),
+          ...state.pets.slice(index + 1)
+        ]
+      })
+    default:
+      return state
+  }
 }
 
-export function dispatch(){
+export function dispatch(action){
+  state = managePets(state, action)
+  render()
 }
 
 export function render(){
-
+  var display = document.getElementById('container')
+  var html = state.pets.map( pet => `<li>${pet.name}</li>`).join('')
+  display.innerHTML = `<ul>${html}</ul>`
 }
